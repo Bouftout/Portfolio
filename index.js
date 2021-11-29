dbbcd41c1010db2b6efe20e636aa237c3f6be363
ToniPortal <tonitoctoc13@gmail.com>
2021-11-29 11:00:19 +0100

    commit

--- index.js
+++ index.js
@@ -14,25 +14,34 @@ app.listen(port)
 */
 
 const path = require('path')
-app.use('/', express.static(path.join(__dirname, 'Page web')))
+app.use('/',  express.static(path.join(__dirname, 'Page web')))
+
+
+app.post('/send',async function (req, res, next) {
+    await main()
+    next();
+  }, function (req, res) {
+    res.redirect('/')
+  });
+  
 
 app.use(helmet())
 app.use(compression())
 
 // server-sent event stream
-app.get('/events', function(req, res) {
+app.get('/events', function (req, res) {
     res.setHeader('Content-Type', 'text/event-stream')
     res.setHeader('Cache-Control', 'no-cache')
 
     // send a ping approx every 2 seconds
-    var timer = setInterval(function() {
+    var timer = setInterval(function () {
         res.write('data: ping\n\n')
 
         // !!! this is the important part
         res.flush()
     }, 2000)
 
-    res.on('close', function() {
+    res.on('close', function () {
         clearInterval(timer)
     })
 });
@@ -57,14 +66,9 @@ console.log(('Langue:' + moment.locale('fr') + 'ançaise\n').silly + //Langue fr
     moment().format('llll').prompt + `Port: ${port}`.info)
 
 
-
-
-
-
-
-app.get('/cv', function(req, res) {
+app.get('/cv', function (req, res) {
     var file = path.join(__dirname, './Page web/cv.pdf');
-    res.download(file, function(err) {
+    res.download(file, function (err) {
         if (err) {
             console.log("Error");
             console.log(err);
@@ -84,35 +88,28 @@ function makeid(length) {
     return result;
 }
 
-
-
-
-
-
-
-
-app.get('/notexist', function(req, res) {
-        console.log('Le fichier ici: '.error + './Page web/notexist/index.html'.warn + ' n\'éxiste pas'.error)
-        console.log('Chargement du message de '.info + '\"Erreur'.error + ' Foetal !\"'.silly)
-        console.log('Cause de l\'erreur: le fichier html qui vous dit que le fichier n\'existe ben lui aussi il n\'hésite pas'.white)
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write('<!DOCTYPE html>' +
-            '<html>' +
-            '    <head>' +
-            '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>' +
-            '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>' +
-            '        <title>Erreur Fatal !</title>' +
-            '    </head>' +
-            '    <body> <div>' +
-            '     	<H3>Vous avez été redirigée ici car le fichier html qui vous dit que le fichier n\'existe pas lui aussi' +
-            '         il n\'existe pas !</H3><br><br> ' +
-            '<a onclick="javascript:window.history.back()" class="btn-large waves-effect waves-light orange">Cliquer ici pour revenir en arrière</a><br><br>' +
-            '<br><br><a href="/" class="btn-large waves-effect waves-light orange">Cliquer ici pour revenir sur l\'acceuil</a><br><br>' +
-            '<H5>Si ce lien juste au dessue recharge la page ça veut dire que  l\'acceuil n\'existe pas,attendez que le dev regle le probléme</H5>' +
-            '    <div> </body>' +
-            '</html>');
-        res.end();
-    }) // fin app get notexists
+app.get('/notexist', function (req, res) {
+    console.log('Le fichier ici: '.error + './Page web/notexist/index.html'.warn + ' n\'éxiste pas'.error)
+    console.log('Chargement du message de '.info + '\"Erreur'.error + ' Foetal !\"'.silly)
+    console.log('Cause de l\'erreur: le fichier html qui vous dit que le fichier n\'existe ben lui aussi il n\'hésite pas'.white)
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write('<!DOCTYPE html>' +
+        '<html>' +
+        '    <head>' +
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>' +
+        '        <title>Erreur Fatal !</title>' +
+        '    </head>' +
+        '    <body> <div>' +
+        '     	<H3>Vous avez été redirigée ici car le fichier html qui vous dit que le fichier n\'existe pas lui aussi' +
+        '         il n\'existe pas !</H3><br><br> ' +
+        '<a onclick="javascript:window.history.back()" class="btn-large waves-effect waves-light orange">Cliquer ici pour revenir en arrière</a><br><br>' +
+        '<br><br><a href="/" class="btn-large waves-effect waves-light orange">Cliquer ici pour revenir sur l\'acceuil</a><br><br>' +
+        '<H5>Si ce lien juste au dessue recharge la page ça veut dire que  l\'acceuil n\'existe pas,attendez que le dev regle le probléme</H5>' +
+        '    <div> </body>' +
+        '</html>');
+    res.end();
+}) // fin app get notexists
 
 
 
@@ -121,7 +118,7 @@ app.get('/notexist', function(req, res) {
 ms = require('mediaserver'); //ms require
 
 //chargement pour toute les musique
-app.get('/portal2end.mp3', function(req, res) {
+app.get('/portal2end.mp3', function (req, res) {
     ms.pipe(req, res, "./Page web/audioplayer/Portal2end.mp3");
 });
 
@@ -133,9 +130,46 @@ var favicon = require('serve-favicon'); // Charge le middleware de favicon
 app.use(morgan('combined')) // Active le middleware de logging
     // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
     .use(favicon(__dirname + '/public/favicon.ico'))
-    .use(function(req, res) { // Répond enfin
-        fs.readFile('./Page web/index.html', 'utf-8', function(error, content) {
+    .use(function (req, res) { // Répond enfin
+        fs.readFile('./Page web/index.html', 'utf-8', function (error, content) {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(content);
         });
     });
+
+var nodemailer = require('nodemailer');
+
+
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'davonte.nader94@ethereal.email',
+            pass: '5qcXNQ6TxV4ys3kSG5'
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Davonte Nader" davonte.nader94@ethereal.email', // sender address
+        to: "tonitoctoc13@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    return "Preview URL: %s", nodemailer.getTestMessageUrl(info)
+}
+
